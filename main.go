cd3d94935e229c18ebde6516b9619bc2cd7f10df
gabrielbismarck <gabrielbismarck7@gmail.com>
2026-07-07 18:46:43 -0300

    remove comments

--- main.go
+++ main.go
@@ -1,36 +1,3 @@
-// package main
-
-// import (
-//     "fmt"
-//     "log"
-//     "github.com/gabrielbismarck/GoMaker/internal/controller"
-//     "github.com/gabrielbismarck/GoMaker/pkg/index"
-//     "github.com/gofiber/fiber/v2"
-// )
-
-// func main() {
-
-//     myIndexer := index.NewIndexer("default")
-
-// 	controller.SetIndexer(myIndexer)
-
-// 	app := fiber.New()
-
-// 	app.Post("/index", controller.AddDocumentToIndex)
-
-// 	app.Post("/save-index", func(c *fiber.Ctx) error {
-// 		if err := myIndexer.SaveIndex(); err != nil {
-// 			log.Printf("Erro ao salvar o índice: %v", err)
-// 			return c.Status(fiber.StatusInternalServerError).SendString(fmt.Sprintf("Erro ao salvar o índice: %v", err))
-// 		}
-// 		return c.SendString("Índice salvo com sucesso!")
-// 	})
-
-// 	log.Fatal(app.Listen(":3000"))
-
-// }
-
-
 package main
 
 import (
@@ -56,7 +23,7 @@ func main() {
     // Rota de Indexação (Parte do Lucas): Recebe os documentos.
     app.Post("/index", controller.AddDocumentToIndex)
 
-    // CORREÇÃO - SUA PARTE: Rota de Busca (Sua responsabilidade).
+    // Rota de Busca.
     // Usamos o método GET pois buscas são consultas de recuperação de dados.
     app.Get("/search", controller.SearchQuery)
 
